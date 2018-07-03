10d1b7f7186abce600c171774dfa0804545ea1ca
Alberto Donato <alberto.donato@gmail.com>
2018-07-03 18:49:44 +0200

    first import

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tiny static web server with TLS and HTTP/2 support
+
+[![Build status](https://img.shields.io/travis/albertodonato/staticserve.svg)](https://travis-ci.org/albertodonato/staticserve)
+[![Go Report Card](https://goreportcard.com/badge/github.com/albertodonato/staticserve)](https://goreportcard.com/report/github.com/albertodonato/staticserve)
+[![GoDoc](https://godoc.org/github.com/albertodonato/staticserve?status.svg)](https://godoc.org/github.com/albertodonato/staticserve)
+
+A minimal HTTP server using the builtin go library. It supports TLS and HTTP/2.
+
+It can be run simply as
+
+```bash
+go run staticserve.go
+```
+
+## Build
+
+Run
+
+```bash
+go build staticserve.go
+```
+
+## HTTPS support
+
+To run the server on HTTPS, a key/certificate pair is required. The service can be run with
+
+```bash
+staticserve -tls-cert cert.pem -tls-key key.pem
+```
---
+++ staticserve.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type flags struct {
+	Addr    string
+	Dir     string
+	Log     bool
+	TLSCert string
+	TLSKey  string
+}
+
+type LogHandler struct {
+	Handler http.Handler
+}
+
+func (lh *LogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s", r.Proto, r.Method, r.URL)
+	lh.Handler.ServeHTTP(w, r)
+}
+
+func parseFlags() (f flags) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	flag.StringVar(&f.Addr, "addr", ":8080", "address and port to listen on")
+	flag.StringVar(&f.Dir, "dir", pwd, "directory to serve")
+	flag.BoolVar(&f.Log, "log", false, "log requests")
+	flag.StringVar(&f.TLSCert, "tls-cert", "", "certificate file for TLS connections")
+	flag.StringVar(&f.TLSKey, "tls-key", "", "key file for TLS connections")
+	flag.Parse()
+	return
+}
+
+func main() {
+	flags := parseFlags()
+	serveTLS := flags.TLSCert != "" && flags.TLSKey != ""
+
+	if flags.Log {
+		kind := "HTTP"
+		if serveTLS {
+			kind = "HTTPS"
+		}
+		log.Printf("Starting %s server on %s, serving path %s", kind, flags.Addr, flags.Dir)
+	}
+
+	handler := http.FileServer(http.Dir(flags.Dir))
+	if flags.Log {
+		handler = &LogHandler{Handler: handler}
+	}
+
+	var err error
+	if serveTLS {
+		err = http.ListenAndServeTLS(flags.Addr, flags.TLSCert, flags.TLSKey, handler)
+	} else {
+		err = http.ListenAndServe(flags.Addr, handler)
+	}
+	log.Fatal(err)
+}
